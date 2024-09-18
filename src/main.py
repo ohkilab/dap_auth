93c8310141068142f8212ee977bced91c83d7c7f
joko0811 <perry_0096@docomo.ne.jp>
2024-09-18 15:59:14 +0900

    change user nameing format

--- src/main.py
+++ src/main.py
@@ -10,18 +10,30 @@ def sampling(user1_name, user2_name, device1_address, device2_address):
     device1_data, device2_data = sampler.get_data()
     device1_data.to_csv(f"{user1_name}.csv")
     device2_data.to_csv(f"{user2_name}.csv")
+    return device1_data, device2_data
 
 
 def main():
-    user1_name = "hoge"
-    user2_name = "huga"
+    print("plese input user1_name: ", end="")
+    user1_name = input()
+    print("plese input user2_name: ", end="")
+    user2_name = input()
+    # black band
     device1_address = "A59901F2-0211-6282-AA8C-4858238B4AE0"
+    # blue band
     device2_address = "F76B7A81-43CD-5515-B7C3-997B6847F307"
+
+    device1_data, device2_data = sampling(
+        user1_name, user2_name, device1_address, device2_address
+    )
+
+
+if __name__ == "__main__":
 
     tracemalloc.start()
     faulthandler.enable()
 
-    sampling(user1_name, user2_name, device1_address, device2_address)
+    main()
 
     snapshot = tracemalloc.take_snapshot()
     top_stats = snapshot.statistics("traceback")
@@ -32,7 +44,3 @@ def main():
         for line in stat.traceback.format():
             print(line)
         print("=====")
-
-
-if __name__ == "__main__":
-    main()
