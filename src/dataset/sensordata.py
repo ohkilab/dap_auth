aa941014ab333b2e495b81bd44e27a25ba5bf040
joko0811 <perry_0096@docomo.ne.jp>
2024-09-27 13:38:18 +0900

    add dataset class

--- src/dataset/sensordata.py
+++ src/dataset/sensordata.py
@@ -87,3 +87,130 @@ class PairDataDataset(BasePairDataset):
         user2_sensor_data = pd.read_csv(user2_sensor_data_path)
 
         return user1_sensor_data, user2_sensor_data, label, data_info
+
+
+class MaeSoDatasetMode(Enum):
+    NORMAL = 0
+    MASTERY = 1
+    SPOOF = 2
+    COLLAB_SPOOF = 3
+
+
+class MaeSoIndivisualDataset(BasePairDataset):
+    def __init__(
+        self,
+        directory_path: str,
+        correct_pair_names: tuple[str, str],
+        mode: MaeSoDatasetMode = MaeSoDatasetMode.NORMAL,
+    ):
+        self.correct_pair_names = correct_pair_names
+        self.mode = mode
+
+        self.id_file_path = os.path.join(directory_path, "id.csv")
+        if not os.path.exists(self.id_file_path):
+            raise ValueError(f"ID file does not exist : {self.id_file_path}")
+
+        # 識別子とユーザー名の対応表
+        # カラム名はid,name
+        # idは{pair_number}_{user_number}の形式
+        self.id_file = pd.read_csv(self.id_file_path)
+
+        self._validate_correct_pair_names()
+
+        self.user1_file_path_list = []
+        self.user2_file_path_list = []
+        self._generate_file_list(directory_path)
+        if len(self.user1_file_path_list) != len(self.user2_file_path_list):
+            raise ValueError("The number of user1 and user2 data is different")
+
+    def _generate_file_list(self, directory_path):
+
+        mode_directory_path = os.path.join(directory_path, self.mode.name.lower())
+
+        pair_dir_name_list = [
+            name
+            for name in os.listdir(mode_directory_path)
+            if os.path.isdir(os.path.join(mode_directory_path, name))
+        ]
+
+        pair_number_list = [
+            item.split("_")[0]
+            for item in pair_dir_name_list
+            if item.split("_")[1] == "1"
+        ]
+        for pair_number in pair_number_list:
+            user1_file_path = os.path.join(mode_directory_path, f"{pair_number}_0")
+            user2_file_path = os.path.join(mode_directory_path, f"{pair_number}_1")
+
+            if os.path.exists(user1_file_path) and os.path.exists(user2_file_path):
+                user1_file_path_list = glob(user1_file_path + "/*.csv")
+                user2_file_path_list = glob(user2_file_path + "/*.csv")
+                file_path_list_length = min(
+                    len(user1_file_path_list), len(user2_file_path_list)
+                )
+                user1_file_path_list = user1_file_path_list[:file_path_list_length]
+                user2_file_path_list = user2_file_path_list[:file_path_list_length]
+
+                self.user1_file_path_list += user1_file_path_list
+                self.user2_file_path_list += user2_file_path_list
+            else:
+                raise ValueError(
+                    f"Pair data file does not exist : {user1_file_path} or {user2_file_path}"
+                )
+
+    def __len__(self):
+        return len(self.user1_file_path_list)
+
+    def _validate_correct_pair_names(self):
+        def df_contain_name(df, name):
+            return name in df.loc[:, "id"].values
+
+        error_msg = ""
+        if not df_contain_name(self.id_file, self.correct_pair_names[0]):
+            error_msg += self.correct_pair_names[0]
+        if not df_contain_name(self.id_file, self.correct_pair_names[1]):
+            if error_msg:
+                error_msg = "(" + error_msg + ", " + self.correct_pair_names[1] + ")"
+            else:
+                error_msg = self.correct_pair_names[1]
+
+        if error_msg != "":
+            raise ValueError(f"User name {error_msg} not found in dataset")
+
+    def path2id(self, path):
+        return os.path.basename(os.path.dirname(path))
+
+    def __getitem__(self, idx):
+        if idx != 0:
+            user1_file_path = self.user1_file_path_list[idx % len(self)]
+            user2_file_path = self.user2_file_path_list[idx % len(self)]
+        else:
+            user1_file_path = self.user1_file_path_list[idx]
+            user2_file_path = self.user2_file_path_list[idx]
+        user1_sensor_data = pd.read_csv(user1_file_path)
+        user2_sensor_data = pd.read_csv(user2_file_path)
+
+        user1_id = self.path2id(user1_file_path)
+        user2_id = self.path2id(user2_file_path)
+
+        data_info = pd.Series(
+            {
+                "user1_id": user1_id,
+                "user2_id": user2_id,
+                "user1_data_path": user1_file_path,
+                "user2_data_path": user2_file_path,
+            }
+        )
+        label = self._get_label(data_info)
+
+        return user1_sensor_data, user2_sensor_data, label, data_info
+
+    def _get_label(self, data_info: pd.Series) -> int:
+        user1_id = data_info["user1_id"]
+        user2_id = data_info["user2_id"]
+
+        if (user1_id in self.correct_pair_names) and (
+            user2_id in self.correct_pair_names
+        ):
+            return 1
+        return 0
