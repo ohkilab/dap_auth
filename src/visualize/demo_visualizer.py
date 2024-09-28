2413867180fc114b3ed0cf698095c007a9b3ea5e
joko0811 <perry_0096@docomo.ne.jp>
2024-09-29 01:43:39 +0900

    add visualizer

---
+++ src/visualize/demo_visualizer.py
@@ -0,0 +1,87 @@
+from enum import Enum
+
+import dash
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+from .sensor_page import SensorDataVisualizer
+
+
+class DemoPageStat(Enum):
+    SAMPLING = 0
+    AUTHORIZE = 1
+
+
+class DemoSite:
+    def __init__(self):
+        # Dashアプリケーションの作成
+        self.app = dash.Dash(__name__)
+        self.init_page()
+        # レイアウトの設定、データ更新関数の登録
+        self.app_layout = self._create_layout()
+        self.app.layout = self.app_layout
+        self.register_callbacks()
+
+    def run(self):
+        self.app.run_server(debug=True)
+
+    def init_page(self):
+        self.state = DemoPageStat.SAMPLING
+        self.old_state = self.state
+        self.sampling_page = SensorDataVisualizer()
+
+    def register_callbacks(self):
+        self.sampling_page.register_callbacks(self.app)
+
+        @self.app.callback(
+            Output("page-content", "children"), Input("page_counter", "n_intervals")
+        )
+        def display_page(n):
+            if self.state == self.old_state:
+                # 描画なし
+                raise dash.exceptions.PreventUpdate
+
+            self.old_state = self.state
+            if self.state == DemoPageStat.SAMPLING:
+                return self.sampling_page.get_layout()
+            elif self.state == DemoPageStat.AUTHORIZE:
+                return self.sampling_page.get_layout()
+            else:
+                return self.sampling_page.get_layout()
+
+    def _create_layout(self):
+        page_layout = self.sampling_page.get_layout()
+        return html.Div(
+            [
+                html.Div(id="page-content", children=page_layout),
+                dcc.Interval(id="page_counter", interval=10000, n_intervals=0),
+            ]
+        )
+
+
+if __name__ == "__main__":
+    visualizer = DemoSite()
+    import threading
+    import time
+
+    class DummyHoge:
+        def __init__(self, me):
+            self.me = me
+
+        def aaaa(self):
+            self.me()
+
+    def update_data():
+        idx = 0
+        while True:
+            hoge = visualizer.sampling_page.device1_graph_component
+            hoge.update_data([idx, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12])
+            idx += 1
+            time.sleep(1)
+
+    def hogehuga():
+        aaa = DummyHoge(update_data)
+        aaa.aaaa()
+
+    threading.Thread(target=hogehuga).start()
+    visualizer.run()
---
+++ src/visualize/sensor_page.py
@@ -0,0 +1,82 @@
+from dash import dcc, html
+from dash.dependencies import Input, Output
+
+from .component.device_component import DeviceComponent
+
+
+class SensorDataVisualizer:
+    def __init__(self):
+        # 表示項目やデータの初期化
+        self.init_view_data()
+        self.device1_graph_component = DeviceComponent("0")
+        self.device2_graph_component = DeviceComponent("1")
+        # レイアウトの設定、データ更新関数の登録
+        self.layout = self._create_layout()
+        # self.register_callbacks()
+
+    def get_layout(self):
+        return self.layout
+
+    def init_view_data(self):
+        self.title = "Sensor Data Visualizer"
+        self.status_message = "initializing..."
+
+    def register_callbacks(self, app):
+        self.device1_graph_component.register_callbacks(app)
+        self.device2_graph_component.register_callbacks(app)
+
+        @app.callback(
+            Output("status-message", "children"),
+            [Input("sensor-vis-graph-update", "n_intervals")],
+        )
+        def update_status_message(n):
+            return self.status_message
+
+    def _create_layout(self):
+        graph1_layout = self.device1_graph_component.get_layout()
+        graph2_layout = self.device2_graph_component.get_layout()
+        return html.Div(
+            style={
+                "height": "100vh",
+                "padding": "20px",
+                "box-sizing": "border-box",
+                "overflow": "hidden",
+            },
+            children=[
+                html.H1(
+                    self.title,
+                    style={
+                        "text-align": "center",
+                        "margin": "0",
+                        "padding": "0",
+                        "margin-bottom": "10px",
+                    },
+                ),
+                html.Div(
+                    style={
+                        "display": "grid",
+                        "grid-template-columns": "1fr 1fr",
+                        "gap": "10px",
+                        "height": "80vh",
+                    },
+                    children=[
+                        graph1_layout[0],
+                        graph2_layout[0],
+                        graph1_layout[1],
+                        graph2_layout[1],
+                    ],
+                ),
+                dcc.Interval(
+                    id="sensor-vis-graph-update", interval=1000, n_intervals=0
+                ),  # 1秒ごとに更新
+                html.Div(
+                    id="status-message",
+                    style={
+                        "text-align": "center",
+                        "margin-top": "10px",
+                        "font-size": "18px",
+                    },
+                    children=self.status_message,
+                ),
+            ],
+        )
