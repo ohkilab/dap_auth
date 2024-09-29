0e8acad5175ab8a3920fb9ec7bc3ee9408b7da49
joko0811 <perry_0096@docomo.ne.jp>
2024-09-29 10:45:26 +0900

    refactor split function

--- src/encapsulate_preprocess.py
+++ src/encapsulate_preprocess.py
@@ -1,9 +1,13 @@
 import pandas as pd
+from tqdm import tqdm
+from omegaconf import DictConfig
 
 from preprocess.pair_data_extraction import pair_extraction
 from feature.fusion import FusionMode, calculate_extract_fusion_futures
 from preprocess.util import removal_gravitational_acceleration
 from feature.extract import standardization, triaxial_attributes_l2norm
+from dataset.sensordata import MaeSoDatasetMode, MaeSoIndivisualDataset, PairDataDataset
+from sampling.device_handler import MotionSegmentDeterminator
 
 
 def preprocessing(
@@ -45,3 +49,75 @@ def feature_extraction(
     )
 
     return feat
+
+
+def extract_feature_from_old_data(cfg: DictConfig):
+
+    dataset = MaeSoIndivisualDataset(
+        cfg.dataset_path,
+        [cfg.correct_user1, cfg.correct_user2],
+        MaeSoDatasetMode.NORMAL,
+    )
+
+    feat_df = pd.DataFrame()
+    label_list = list()
+    pair_list = list()
+    for device1_data, device2_data, label, data_info in tqdm(dataset):
+        # Calculation of statistical features
+        device1_data, device2_data = pair_extraction(
+            device1_data=device1_data, device2_data=device2_data
+        )
+        feat = calculate_extract_fusion_futures(
+            device1_data, device2_data, FusionMode.FEATURE_MEAN
+        )
+        feat_df = pd.concat([feat_df, feat], axis=0)
+        label_list.append(label)
+        pair_list.append((data_info["user1_id"], data_info["user2_id"]))
+
+    feat_df = feat_df.reset_index().drop("index", axis=1)
+    return feat_df, label_list, pair_list
+
+
+def extract_feature(cfg: DictConfig):
+    dataset = PairDataDataset(cfg.dataset_path, [cfg.correct_user1, cfg.correct_user2])
+
+    def split_sensor_data(sensor_data: pd.DataFrame) -> list[pd.DataFrame]:
+        df_list = []
+        segment_determinator = MotionSegmentDeterminator()
+        for i in range(len(sensor_data)):
+            current_gyro = sensor_data.loc[i, ["gyroX", "gyroY", "gyroZ"]]
+            segment_determinator.updateData(current_gyro, i)
+            if segment_determinator.finished:
+                start_idx = segment_determinator.start_idx
+                end_idx = segment_determinator.end_idx
+                df_list.append(sensor_data.iloc[start_idx:end_idx])
+                segment_determinator.clear()
+        return df_list
+
+    feat_df = pd.DataFrame()
+    label_list = list()
+    pair_list = list()
+    for device1_data, device2_data, label, data_info in tqdm(dataset):
+        # Calculation of statistical features
+        device1_motion_data_list = split_sensor_data(device1_data)
+        device2_motion_data_list = split_sensor_data(device2_data)
+
+        min_length = min(len(device1_motion_data_list), len(device2_motion_data_list))
+        device1_motion_data_list = device1_motion_data_list[:min_length]
+        device2_motion_data_list = device2_motion_data_list[:min_length]
+
+        for device1_motion_data, device2_motion_data in zip(
+            device1_motion_data_list, device2_motion_data_list
+        ):
+            preprocessed_device1_data, preprocessed_device2_data = preprocessing(
+                device1_motion_data, device2_motion_data
+            )
+            feat = feature_extraction(
+                preprocessed_device1_data, preprocessed_device2_data
+            )
+            feat_df = pd.concat([feat_df, feat], axis=0)
+            label_list.append(label)
+            pair_list.append((data_info["user1_name"], data_info["user2_name"]))
+
+    feat_df = feat_df.reset_index().drop("index", axis=1)
+    return feat_df, label_list, pair_list
--- src/train.py
+++ src/train.py
@@ -1,95 +1,16 @@
 import os
-import yaml
 import pickle
 
 import pandas as pd
 import hydra
 from omegaconf import DictConfig
-from tqdm import tqdm
 
-from encapsulate_preprocess import preprocessing, feature_extraction
-from sampling.device_handler import MotionSegmentDeterminator
-from preprocess.pair_data_extraction import pair_extraction
-from feature.fusion import FusionMode, calculate_extract_fusion_futures
 from model.load import load_model
-from dataset.sensordata import PairDataDataset, MaeSoIndivisualDataset, MaeSoDatasetMode
-
-
-def split_sensor_data(sensor_data: pd.DataFrame) -> list[pd.DataFrame]:
-    df_list = []
-    segment_determinator = MotionSegmentDeterminator()
-    for i in range(len(sensor_data)):
-        current_gyro = sensor_data.loc[i, ["gyroX", "gyroY", "gyroZ"]]
-        segment_determinator.updateData(current_gyro, i)
-        if segment_determinator.finished:
-            start_idx = segment_determinator.start_idx
-            end_idx = segment_determinator.end_idx
-            df_list.append(sensor_data.iloc[start_idx:end_idx])
-            segment_determinator.clear()
-    return df_list
-
-
-def extract_feature(cfg: DictConfig):
-    dataset = PairDataDataset(cfg.dataset_path, [cfg.correct_user1, cfg.correct_user2])
-
-    feat_df = pd.DataFrame()
-    label_list = list()
-    pair_list = list()
-    for device1_data, device2_data, label, data_info in tqdm(dataset):
-        # Calculation of statistical features
-        device1_motion_data_list = split_sensor_data(device1_data)
-        device2_motion_data_list = split_sensor_data(device2_data)
-
-        min_length = min(len(device1_motion_data_list), len(device2_motion_data_list))
-        device1_motion_data_list = device1_motion_data_list[:min_length]
-        device2_motion_data_list = device2_motion_data_list[:min_length]
-
-        for device1_motion_data, device2_motion_data in zip(
-            device1_motion_data_list, device2_motion_data_list
-        ):
-            preprocessed_device1_data, preprocessed_device2_data = preprocessing(
-                device1_motion_data, device2_motion_data
-            )
-            feat = feature_extraction(
-                preprocessed_device1_data, preprocessed_device2_data
-            )
-            feat_df = pd.concat([feat_df, feat], axis=0)
-            label_list.append(label)
-            pair_list.append((data_info["user1_name"], data_info["user2_name"]))
-
-    feat_df = feat_df.reset_index().drop("index", axis=1)
-    return feat_df, label_list, pair_list
-
-
-def extract_feature_from_old_data(cfg: DictConfig):
-
-    dataset = MaeSoIndivisualDataset(
-        cfg.dataset_path,
-        [cfg.correct_user1, cfg.correct_user2],
-        MaeSoDatasetMode.NORMAL,
-    )
-
-    feat_df = pd.DataFrame()
-    label_list = list()
-    pair_list = list()
-    for device1_data, device2_data, label, data_info in tqdm(dataset):
-        # Calculation of statistical features
-        device1_data, device2_data = pair_extraction(
-            device1_data=device1_data, device2_data=device2_data
-        )
-        feat = calculate_extract_fusion_futures(
-            device1_data, device2_data, FusionMode.FEATURE_MEAN
-        )
-        feat_df = pd.concat([feat_df, feat], axis=0)
-        label_list.append(label)
-        pair_list.append((data_info["user1_id"], data_info["user2_id"]))
-
-    feat_df = feat_df.reset_index().drop("index", axis=1)
-    return feat_df, label_list, pair_list
+from encapsulate_preprocess import extract_feature_from_old_data
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="train")
-def main(cfg: DictConfig):
+def train(cfg: DictConfig):
     assert (
         cfg.correct_user1 is not None
     ), "Please specify correct_user1. how to use: correct_user1=xxx"
@@ -120,4 +41,4 @@ def main(cfg: DictConfig):
 
 
 if __name__ == "__main__":
-    main()
+    train()
